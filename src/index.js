8937bfd91e2394dfc8e2b54a72952b4a54f56fce
Aleksandr Polyakov <aleksandrpoliakoff@yandex.ru>
2025-02-28 01:44:58 +0300

    finish

--- src/index.js
+++ src/index.js
@@ -3,22 +3,42 @@ import './style/font.scss'
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
-import 'swiper/scss/autoplay';
 import './style/main.scss'
 
 import Swiper from 'swiper';
 import { Navigation, Pagination, Autoplay } from 'swiper/modules';
 
 const swiper = new Swiper('.swiper', {
-  modules: [Navigation, Pagination, Autoplay],
+  modules: [Navigation, Pagination],
  spaceBetween: 30,
       centeredSlides: true,
-      autoplay: {
-        delay: 3000,
-        disableOnInteraction: false,
-      },
       pagination: {
         el: ".swiper-pagination",
         clickable: true,
       },
 });
+
+const timer = document.querySelector(".discount__timer");
+
+var minute = 10;
+var sec = 0;
+setInterval(function(){
+    timer.innerHTML = `
+    <span>00</span>
+    <span>:</span>
+    <span>${minute}</span>
+    <span>:</span>
+    <span>${sec}</span>
+    `;
+    sec--;
+    if(sec < 0){
+        minute--;
+        sec = 59;
+    }
+    if(sec <=9){
+        sec = "0" + sec;
+    }
+    if(sec == 0 && minute == 0){
+        minute = 10;
+    }
+}, 1000);
