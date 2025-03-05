8e40c4082bc3d3073024923da905d903f5690898
Aleksandr Polyakov <aleksandrpoliakoff@yandex.ru>
2025-03-05 21:04:37 +0300

    fix slider

--- src/index.js
+++ src/index.js
@@ -6,23 +6,42 @@ import 'swiper/css/pagination';
 import './style/main.scss'
 
 import Swiper from 'swiper';
-import { Navigation, Pagination, Autoplay } from 'swiper/modules';
+import { Navigation, Pagination } from 'swiper/modules';
+
 
 const swiper = new Swiper('.swiper', {
-  modules: [Navigation, Pagination],
- spaceBetween: 30,
-      centeredSlides: true,
-      pagination: {
+    modules: [Navigation, Pagination],
+    spaceBetween: 30,
+    slidesPerGroup: 1,
+    centeredSlides: true,
+    pagination: {
         el: ".swiper-pagination",
         clickable: true,
-      },
+    },
 });
+
+
+function widthListener() {
+    let width = document.body.clientWidth;
+
+    if (width >= 768) {
+        swiper.destroy()
+    }
+}
+
+widthListener();
+
+window.addEventListener('resize', () => {
+    widthListener();
+});
+
+
 
 const timer = document.querySelector(".discount__timer");
 
 var minute = 10;
 var sec = 0;
-setInterval(function(){
+setInterval(function () {
     timer.innerHTML = `
     <span>00</span>
     <span>:</span>
@@ -31,14 +50,14 @@ setInterval(function(){
     <span>${sec}</span>
     `;
     sec--;
-    if(sec < 0){
+    if (sec < 0) {
         minute--;
         sec = 59;
     }
-    if(sec <=9){
+    if (sec <= 9) {
         sec = "0" + sec;
     }
-    if(sec == 0 && minute == 0){
+    if (sec == 0 && minute == 0) {
         minute = 10;
     }
 }, 1000);
